a4febe9e26e9a2e6b090c7d741c013d7636b915b
kumaradji <kumaradji@gmail.com>
2024-01-04 18:30:36 +0300

    Initial commit

---
+++ publicposts/publicposts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from publicpostapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/posts/', views.posts),
+    path('api/like_post/<int:post_id>/', views.like_post),
+]
